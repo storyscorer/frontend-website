8cb13384a07bef487f26ca850568051006e38685
jamiemcl001 <jamiemcl001@gmail.com>
2022-06-27 20:24:27 +0100

    chore: add ReactHelmet and body class

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Helmet from "react-helmet";
 import "./App.css";
 
 function App() {
-  return <div className="bg-slate-100"></div>;
+  return (
+    <>
+      <Helmet bodyAttributes={{ class: "bg-slate-600" }}></Helmet>
+      <div className="bg-slate-100"></div>
+    </>
+  );
 }
 
 export default App;
